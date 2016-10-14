70dc3dfac2581002277dccf03bc4dd719508ffd4
Axtral <alexandre.caron@etu.univ-amu.fr>
2016-10-14 11:02:25 +0200

    Création de l'agencement de base de CMatrice

--- src/test2/CMatrice.java
+++ src/test2/CMatrice.java
@@ -4,4 +4,19 @@ package test2;
  * Created by c14012299 on 14/10/2016.
  */
 public class CMatrice {
-}
+
+    public CMatrice(){
+
+        int[][] CMat = new  int[3][4];
+
+    }// Crée la matrice qui sera utilisé pour la Translation et la Rotation
+
+    public void Rotation(CMatrice Mat){
+
+    }
+
+    public void Translation(CMatrice Trans){
+
+    }
+
+}//Classe Matrice qui contiendras tt les fonction liée au mathématique
