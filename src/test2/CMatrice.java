5ce16425b3491bd2300899901b9bbc48133fa3e6
Axtral <alexandre.caron@etu.univ-amu.fr>
2016-10-14 11:47:35 +0200

    Pb ds les matrice

--- src/test2/CMatrice.java
+++ src/test2/CMatrice.java
@@ -5,17 +5,43 @@ package test2;
  */
 public class CMatrice {
 
-    public CMatrice(){
+    private int[][] CMat = new  int[3][4];
 
-        int[][] CMat = new  int[3][4];
+    /*public CMatrice(){
 
-    }// Crée la matrice qui sera utilisé pour la Translation et la Rotation
+
+
+    }// Crée la matrice qui sera utilisé pour la Translation et la Rotation*/
 
     public void Rotation(CMatrice Mat){
 
     }
 
-    public void Translation(CMatrice Trans){
+    public void Translation(CMatrice Mat){
+
+    }
+
+    public void Test(){
+
+        int Cpt = 0;
+
+        //CMatrice Mat = new CMatrice();
+
+        //CMat[2][3] = 1;
+
+        /*int[] tab = new int[5];
+
+        for (int i = 0; i < 5 ; ++i ){
+            tab[i] = ++Cpt;
+        }*/
+
+
+    }
+
+    public static void main(String[] args){
+
+
+
 
     }
 
